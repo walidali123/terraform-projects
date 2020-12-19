97a1f767e35f0a12e66be4e295829e9908f130d1
Nana Janashia <nn_janashia@yahoo.com>
2020-12-19 20:40:46 +0100

    fix eks config

--- vpc.tf
+++ vpc.tf
@@ -24,16 +24,16 @@ module "myapp-vpc" {
     enable_dns_hostnames = true
 
     tags = {
-        "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
     }
 
     public_subnet_tags = {
-        "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
         "kubernetes.io/role/elb" = 1 
     }
 
     private_subnet_tags = {
-        "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
         "kubernetes.io/role/internal-elb" = 1 
     }
 
