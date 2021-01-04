39814a904a95b0d1aecdec988b77fc2f7d13e939
Nana Janashia <nn_janashia@yahoo.com>
2021-01-04 18:42:42 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ output "ami_id" {
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-      Name: "${var.env_prefix}-vpc"
+      Name = "${var.env_prefix}-vpc"
   }
 }
 
@@ -39,7 +39,7 @@ resource "aws_subnet" "myapp-subnet-1" {
   cidr_block = var.subnet_1_cidr_block
   availability_zone = var.avail_zone
   tags = {
-      Name: "${var.env_prefix}-subnet-1"
+      Name = "${var.env_prefix}-subnet-1"
   }
 }
 
