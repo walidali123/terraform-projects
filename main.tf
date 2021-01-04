93673ce063c9c9de353b41897469e8dfd203ee3c
Nana Janashia <nn_janashia@yahoo.com>
2021-01-04 16:55:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-provider "aws" {}
+provider "aws" {
+    region = "eu-west-3"
+}
 
 variable cidr_blocks {
     description = "cidr blocks and name tags for vpc and subnets"
@@ -23,6 +25,19 @@ resource "aws_subnet" "myapp-subnet-1" {
     availability_zone = var.avail_zone
     tags = {
         Name = var.cidr_blocks[1].name
+    }
+}
+
+data "aws_vpc" "existing_vpc" {
+    default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id = data.aws_vpc.existing_vpc.id
+    cidr_block = "172.31.48.0/20"
+    availability_zone = "eu-west-3a"
+    tags = {
+        Name = "subnet-2-default"
     }
 }
 
