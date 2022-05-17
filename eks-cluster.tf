e681aecd1637f382afa4a37cd3a36a8701151fb4
Nana Janashia <nn_janashia@yahoo.com>
2022-05-17 14:13:37 +0200

    Update eks module

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,43 +1,25 @@
-provider "kubernetes" {
-    load_config_file = "false"
-    host = data.aws_eks_cluster.myapp-cluster.endpoint
-    token = data.aws_eks_cluster_auth.myapp-cluster.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
-}
-
-data "aws_eks_cluster" "myapp-cluster" {
-    name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "myapp-cluster" {
-    name = module.eks.cluster_id
-}
-
 module "eks" {
-    source = "terraform-aws-modules/eks/aws"
-    version = "13.2.1"
-    
-    cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.19"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.21.0"
 
-    subnets = module.myapp-vpc.private_subnets
-    vpc_id = module.myapp-vpc.vpc_id
+  cluster_name = "myapp-eks-cluster"  
+  cluster_version = "1.22"
 
-    tags = {
-        environment = "development"
-        application = "myapp"
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id = module.myapp-vpc.vpc_id
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t2.small"]
     }
-    
-    worker_groups = [
-        {
-            instance_type = "t2.small"
-            name = "worker-group-1"
-            asg_desired_capacity = 2
-        },
-        {
-            instance_type = "t2.medium"
-            name = "worker-group-2"
-            asg_desired_capacity = 1
-        }
-    ]
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "eu-west-3"
+    region = "eu-west-2"
 }
 
 variable vpc_cidr_block {}
